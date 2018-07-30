01b2a939fc98991483d975b9dd2b7e886b5dee46
43818 <43818@win438180627.dahuatech.com>
2018-07-30 15:28:51 +0800

    delete useless

---
+++ src/main/java/cn/tang/cloud/servlet/ServletTest.java
@@ -0,0 +1,24 @@
+package cn.tang.cloud.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletTest extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+        resp.getWriter().write("自定义 Servlet");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+}
